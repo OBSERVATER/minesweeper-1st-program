eff77ca611e209b4e709fe815f26cd4948945d6c
PiPiniu <3254140082@qq.com>
2023-12-13 23:36:52 +0800

    实现了按钮

--- src/chess.java
+++ src/chess.java
@@ -1,9 +1,30 @@
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 import java.io.FileInputStream;
 
-public class chess{
+public class chess extends Frame{
+    Image imgnormal = Toolkit.getDefaultToolkit().getImage("res/normal.png");
+    Image imgsafe = Toolkit.getDefaultToolkit().getImage("res/safe0.png");
+    private boolean isBomb;
+    private boolean ispressed = false;
+    private int rx;
+    private int ry;
+
+    public void pressed(int x,int y){
+        System.out.println(x + "<---->" + y);
+        if (x <= 26 && y <=116){
+            ispressed = true;
+        }
+    }
+    public void render(Graphics g){
+        if (ispressed)
+            g.drawImage(imgsafe,10,100,16,16,this);
+        else
+            g.drawImage(imgnormal,10,100,16,16,this);
+    }
 
 }
--- src/launchFrame.java
+++ src/launchFrame.java
@@ -1,15 +1,46 @@
 import javax.swing.*;
 import java.awt.*;
-public class launchFrame extends JFrame {
-    int time = 0;
-    private static final int WIDTH = 640;
-    private static final int HEIGHT = 480;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class launchFrame extends Frame implements MouseListener{
+    public static final int WIDTH = 640;
+    public static final int HEIGHT = 480;
     private Image offscreenimage = null;
+    int time = 0;
+    chess a = new chess();
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        System.out.println("鼠标dianji");
+        a.pressed(e.getX(),e.getY());
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
     private class time_Thread implements Runnable{
         public void run(){
             while (true) {
                 try {
-                    repaint();
                     Thread.sleep(1000);
                     time++;
                 } catch (InterruptedException e) {
@@ -32,14 +63,20 @@ public class launchFrame extends JFrame {
     }
     public void win() {
         setSize(WIDTH, HEIGHT);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
         setLocationRelativeTo(null);
+        setTitle("Minecraft");
         setVisible(true);
         setResizable(false);
         Thread counter = new Thread(new time_Thread());
         counter.start();
-        //Thread render = new Thread(new paint_Thread());
-        //render.start();
+        Thread render = new Thread(new paint_Thread());
+        render.start();
+        addMouseListener(this);
     }
 
     public void update(Graphics g) {
@@ -47,16 +84,18 @@ public class launchFrame extends JFrame {
             this.offscreenimage = this.createImage(WIDTH,HEIGHT);
         }
         Graphics Goff = offscreenimage.getGraphics();
-        Goff.setColor(Color.blue);
+        Color c = Goff.getColor();
+        Goff.setColor(Color.GRAY);
+        Goff.fillRect(0,0,WIDTH,HEIGHT);
+        Goff.setColor(c);
         paint(Goff);
         g.drawImage(this.offscreenimage,0,0,null);
     }
 
     public void paint(Graphics g) {
-        g.setColor(Color.WHITE);
-        g.fillRect(0,0,WIDTH,HEIGHT);
-        g.setColor(Color.BLUE);
+        g.setColor(Color.blue);
         g.drawString("Time: " + this.time, 10, 40);
+        a.render(g);
     }
 
     public static void main(String[] args) {
